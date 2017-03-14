f09b482f07af763e395f7d05a4fbdc58ea0cbe2c
Steven-The-Great <stevenator5555@gmail.com>
2017-03-14 18:07:21 -0500

    Fixed shadows

--- shootemup/Block.js
+++ shootemup/Block.js
@@ -3,38 +3,38 @@ function Block(wid, hei, X, Y) {
 	var y = Y;
 	var width = wid;
 	var height = hei;
-	
+
 	var hypTL, hypTR, hypBL, hypBR, oppTL, oppTR, oppBL, oppBR, adjTL, adjTR, adjBL, adjBR;
-	
+
 	function shadow( lightX, lightY) {
 		hypTL = Math.pow((Math.pow(x - lightX, 2) + Math.pow(y - lightY, 2)), 0.5);
 		adjTL = x - lightX;
 		oppTL = y - lightY;
-		
+
 		hypTR = Math.pow((Math.pow(x + width - lightX, 2) + Math.pow(y - lightY, 2)), 0.5);
 		adjTR = x + width - lightX;
 		oppTR = y - lightY;
-		
+
 		hypBL = Math.pow((Math.pow(x - lightX, 2) + Math.pow(y + height - lightY, 2)), 0.5);
 		adjBL = x - lightX;
 		oppBL = y + height - lightY;
-		
+
 		hypBR = Math.pow((Math.pow(x + width - lightX, 2) + Math.pow(y + height - lightY, 2)), 0.5);
 		adjBR = x + width - lightX;
 		oppBR = y + height - lightY;
-		
+
 		fill(0, 4, 145);
-		quad(x,y,698*(adjTL/hypTL),698*(oppTL/hypTL),698*(adjBR/hypBR),698*(oppBR/hypBR),x + width,y + height);
-		quad(x + width,y,698*(adjTR/hypTR),698*(oppTR/hypTR),698*(adjBL/hypBL),698*(oppBL/hypBL),x,y + height);
+		quad(x,y,lightX + 700*(adjTL/hypTL),lightY + 700*(oppTL/hypTL),lightX + 700*(adjBR/hypBR),lightY + 700*(oppBR/hypBR),x + width,y + height);
+		quad(x + width,y,lightX + 700*(adjTR/hypTR),lightY + 700*(oppTR/hypTR),lightX + 700*(adjBL/hypBL),lightY + 700*(oppBL/hypBL),x,y + height);
 	}
-	
+
 	function show() {
     fill(30, 64, 205);
 // 		fill(0, 34, 175);
 		noStroke();
     rect(x, y, width, height);
 	}
-	
+
 	function getProp() {
 		return {
 			x,
@@ -43,7 +43,7 @@ function Block(wid, hei, X, Y) {
 			height
 		}
 	}
-	
+
 	return {
 		shadow,
 		show,
--- shootemup/index.js
+++ shootemup/index.js
@@ -12,7 +12,7 @@ var levelNum = 0;
 
 var levels = [
 	[
-		[[1,5,1]],
+		[[1,1,1]],
 		[[1,1,1]],
 		[[1,1,1]],
 		[[1,5,1]],
@@ -28,11 +28,11 @@ var levels = [
 
 function setup() {
   createCanvas(WIDTH, HEIGHT);
-	
+
 	p = new Protag();
-	
+
 	prop = p.getProp();
-	
+
 	for (let i = 0; i < 5; i++) {
 			let enemy;
 			let rNum = Math.random();
@@ -54,7 +54,7 @@ function setup() {
 		}
 		currentY += scl;
 	}
-	
+
 }
 
 
@@ -66,45 +66,45 @@ function setup() {
 
 function draw() {
 	if(levelNum === -1) {
-		
+
 		background(0, 34, 175);
-		
+
 	} else if(levelNum === 0) {
-		
+
 		background(0, 34, 175);
-		
+
 		p.accelerate();
-			
-  
+
+
   if (keyIsDown(65)  && prop.dead === false) p.goLeft();
 	if (keyIsDown(68) && prop.dead === false) p.goRight();
 	if (keyIsDown(87)    && prop.dead === false) p.goUp();
 	if (keyIsDown(83)  && prop.dead === false) p.goDown();
 	if (keyIsDown(65) !== true && keyIsDown(68) !== true || prop.dead) p.xStop();
 	if (keyIsDown(87) !== true && keyIsDown(83) !== true || prop.dead) p.yStop();
-	
+
 	translate(width/2-prop.x-prop.width/2, height/2-prop.y-prop.height/2);
-	
+
 	if(mouseIsPressed && prop.dead === false && prop.loaded) {
 		p.load();
 		pewPews[pewPews.length] = PewPew(prop.x + prop.width/2, prop.y + prop.height/2, mouseX-width/2+prop.x+prop.width/2, mouseY-height/2+prop.y+prop.height/2);
 		setTimeout(p.load, prop.rate);
 	}
-		
-		p.shadow(-150,-150);
-		
+
+		// p.shadow(-150,-150);
+
 		for(let i = 0; i < antags.length; i++) {
-			antags[i].shadow(-150,-150);
+			antags[i].shadow(p.getProp().x + scl/2,p.getProp().y + scl/2);
 		}
-		
+
 		for(let i = 0; i < levels[levelNum].length; i++) {
 		for(let j = 0; j < levels[levelNum][i].length; j++) {
 			if(levels[levelNum][i][j].shadow !== undefined) {
-				levels[levelNum][i][j].shadow(-150,-150);
+				levels[levelNum][i][j].shadow(p.getProp().x + scl/2,p.getProp().y + scl/2);
 			}
 		}
 	}
-		
+
 		for(let i = 0; i < levels[levelNum].length; i++) {
 		for(let j = 0; j < levels[levelNum][i].length; j++) {
 			if(levels[levelNum][i][j].show !== undefined) {
@@ -112,7 +112,7 @@ function draw() {
 			}
 		}
 	}
-	
+
 	for(let i = 0; i < pewPews.length; i++) {
 		var pewProp = pewPews[i].getProp()
 		pewPews[i]. move();
@@ -121,7 +121,7 @@ function draw() {
 			pewPews.splice(i, 1);
 		}
 	}
-		
+
 	for(let i = 0; i < antags.length; i++) {
 		if(antags[i].getProp().dead === false) {
 			antags[i].move();
@@ -133,20 +133,20 @@ function draw() {
 			if(rNum <= 0.25) enemy = Antag(random(prop.x-prop.width/2-width/2-scl*2.5, prop.x-prop.width/2-width/2-scl*1.5), random(prop.y-prop.height/2-height/2, prop.y-prop.height/2+height/2));
 			else if(0.25 < rNum && rNum <= 0.5) {
 				enemy = Antag(random(prop.x-prop.width/2+width/2+scl*1.5, prop.x-prop.width/2+width/2+scl*2.5), random(prop.y-prop.height/2-height/2, prop.y-prop.height/2+height/2));
-			} 
+			}
 			else if(0.5 < rNum && rNum <= 0.75) enemy = Antag(random(prop.x-prop.width/2-width/2, prop.x-prop.width/2+width/2), random(prop.y-prop.height/2-height/2-scl*2.5, prop.y-prop.height/2-height/2-scl*1.5));
 			else if(0.75 < rNum && rNum <= 1) enemy = Antag(random(prop.x-prop.width/2-width/2, prop.x-prop.width/2+width/2), random(prop.y-prop.height/2+height/2+scl*1.5, prop.y-prop.height/2+height/2+scl*2.5));
 			antags.splice(i,1,enemy);
-			
+
 		}
 	}
-	
+
 	p.show();
 	p.checkCollision();
-	
+
 	p.showScore();
-		
+
 // 		rect(-150,-150,10,10);
-		
+
 	}
 }
